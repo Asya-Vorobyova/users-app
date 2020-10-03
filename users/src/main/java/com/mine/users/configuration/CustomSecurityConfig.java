cf2114829a061bf7974e7860e6cd1cb85f1b3e9e
Axinya Vorobyova <avo@tradeshift.com>
2020-10-03 19:42:25 +0200

    Add basic security; unfortunately tests are broken, have no time to fix them

---
+++ users/src/main/java/com/mine/users/configuration/CustomSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.mine.users.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("adminPass"))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- users/src/test/java/com/mine/users/UserControllerTests.java
+++ users/src/test/java/com/mine/users/UserControllerTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,7 +21,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+//@Import(CustomSecurityConfig.class)
 public class UserControllerTests {
 
     @Autowired
@@ -33,6 +35,7 @@ public class UserControllerTests {
     private UserService userService;
 
     @Test
+    @WithMockUser(username = "admin", password = "adminPass", roles = "ADMIN")
     void whenInputIsOk_thenReturnsStatus200() throws Exception {
         UserDTO userDTO = new UserDTO("123", "Jim", "Morrison");
         String body = objectMapper.writeValueAsString(userDTO);
